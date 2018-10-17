5b16240ee4dfbad66203cbbf224b2ed338b37306
Elie Steinbock <eliesteinbock@gmail.com>
2018-10-17 10:59:21 +0300

    updated migrations to deploy hackathon voting contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var HackathonVoting = artifacts.require("./HackathonVoting.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+  deployer.deploy(HackathonVoting);
 };
